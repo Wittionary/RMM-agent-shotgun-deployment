61e23464f4828908babe70a98d03e97942e30043
Witt Allen <qwert59@gmail.com>
2017-12-14 11:15:27 -0600

    Somethings work and I'm actually making progress

--- RMM agent shotgun deployment.ps1
+++ RMM agent shotgun deployment.ps1
@@ -1,3 +1,9 @@
+Param(
+    # Options: Servers, Workstations, Both, All (both and all are the same)
+    [Parameter(Mandatory = $True, Position = 0)]
+    [string]$scope 
+)
+
 <# --------------------------------------------------------------------------------------------------------------
 RMM agent shotgun deployment
 Version: 0.0.1
@@ -7,16 +13,19 @@ Objective: Remotely install the designated Kaseya agent on computers that have r
 DEPENDANCIES & ASSUMPTIONS:
 - Script will be ran as Administrator
 - Script will be ran on a Domain Controller
-- Server's OS can use WMI filtering
+- OS is Windows Server 2008 or higher (PsExec req.)
 - Imported modules are supported on Server's OS
 - Server has .NET installed
-- Depends on 'psexec' PowerShell module
+- Depends on 'psexec' PowerShell module   -------------- Maybe not
+
 
 GENERAL TODO:
 - Create event log entries when stuff happens
 # --------------------------------------------------------------------------------------------------------------
 #>
 
+
+<# ---------------------------------------- This comment block can be removed if still commented out at production
 $repoURL = "https://github.com/replicaJunction/PsExec/blob/master/PsExec/PsExec.psm1"
 $repoPath =
 $tempPath = "C:\temp"
@@ -28,21 +37,21 @@ if (Install-Module -Name psexec -RequiredVersion 0.0.7) {
     # Do nothing
 }
 else {
-       <# SOURCE: https://github.com/MSAdministrator/GetGithubRepository
-        .Synopsis
-        This function will download a Github Repository without using Git
-        .DESCRIPTION
-        This function will download files from Github without using Git.  You will need to know the Owner, Repository name, branch (default master),
-        and FilePath.  The Filepath will include any folders and files that you want to download.
-        .EXAMPLE
-        Get-GithubRepository -Owner MSAdministrator -Repository WriteLogEntry -Verbose -FilePath `
-                'WriteLogEntry.psm1',
-                'WriteLogEntry.psd1',
-                'Public',
-                'en-US',
-                'en-US\about_WriteLogEntry.help.txt',
-                'Public\Write-LogEntry.ps1'
-        #>
+       # SOURCE: https://github.com/MSAdministrator/GetGithubRepository
+       # .Synopsis
+       # This function will download a Github Repository without using Git
+       # .DESCRIPTION
+       # This function will download files from Github without using Git.  You will need to know the Owner, Repository name, branch (default master),
+       # and FilePath.  The Filepath will include any folders and files that you want to download.
+       # .EXAMPLE
+       # Get-GithubRepository -Owner MSAdministrator -Repository WriteLogEntry -Verbose -FilePath `
+       #         'WriteLogEntry.psm1',
+       #         'WriteLogEntry.psd1',
+       #         'Public',
+       #         'en-US',
+       #         'en-US\about_WriteLogEntry.help.txt',
+       #         'Public\Write-LogEntry.ps1'
+        
     function Get-GithubRepository {
         [CmdletBinding()]
         [Alias()]
@@ -123,5 +132,79 @@ else {
         'appveyor.yml'    
     Import-Module $repoPath
 }
+#>
 
+# ------------------------------- Query AD for PCs that connected in last 30 days and write to file-------------------------------
+# TODO: Test each string case, may not operate as expected
+$scope = $scope.ToLower()
+$daysAgo = (Get-Date).AddDays(-30)
+$listPath = "C:\temp\ComputerList.txt"
 
+if ($scope -like "*server*") {
+    $dcs = Get-ADComputer -Filter { (lastLogonDate -gt $daysAgo) -and (OperatingSystem -Like '*Server*')} -Properties lastLogonDate
+
+    foreach ($dc in $dcs) { 
+        Get-ADComputer $dc.Name -Properties lastlogontimestamp | 
+            Select-Object @{n = "Computer"; e = {$_.Name}}, @{Name = "Lastlogon"; Expression = {[DateTime]::FromFileTime($_.lastLogonTimestamp)}} |
+            Out-File -FilePath $listPath -Append
+    }
+}
+elseif ($scope -like "*workstation*") {
+    $dcs = Get-ADComputer -Filter { (lastLogonDate -gt $daysAgo) -and (OperatingSystem -NotLike '*Server*')} -Properties lastLogonDate
+
+    foreach ($dc in $dcs) { 
+        Get-ADComputer $dc.Name -Properties lastlogontimestamp | 
+            Select-Object @{n = "Computer"; e = {$_.Name}}, @{Name = "Lastlogon"; Expression = {[DateTime]::FromFileTime($_.lastLogonTimestamp)}} |
+            Out-File -FilePath $listPath -Append
+    }
+}
+elseif (($scope -like "*both*") -or ($scope -like "*all*")) {
+    $dcs = Get-ADComputer -Filter {lastLogonDate -gt $daysAgo} -Properties lastLogonDate
+
+    foreach ($dc in $dcs) { 
+        Get-ADComputer $dc.Name -Properties lastlogontimestamp | 
+            Select-Object @{n = "Computer"; e = {$_.Name}}, @{Name = "Lastlogon"; Expression = {[DateTime]::FromFileTime($_.lastLogonTimestamp)}} |
+            Out-File -FilePath $listPath -Append
+    }
+}
+else {
+    # TODO: Exit script with failure code
+}
+
+# ------------------------------------- Download RMM agent
+$vsaURL = "https://vsa.data-blue.com"
+$agentEXE = "KcsSetup.exe"
+$agentSwitches = " /e /g=root." + $organizationID + " /c /j /s" # Switches: http://help.kaseya.com/WebHelp/EN/VSA/9040000/#493.htm
+$url = $vsaURL + "/install/VSA-default--1/" + $agentEXE
+$output = "C:\$agentEXE"
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($url, $output) 
+
+# ------------------------------------- Download PsExec
+$timeout = 5 # Seconds for psexec to spend attempting to connect to remote PC
+$url = "https://download.sysinternals.com/files/PSTools.zip"
+$psToolsZip = "C:\temp\PSTools.zip"
+$psToolsPath = "C:\temp\PSTools\"
+$psExecPath = "C:\temp\PSTools\psexec.exe"
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($url, $psToolsZip)     
+
+# Unzip the PSTools package
+# Source: https://stackoverflow.com/questions/27768303/how-to-unzip-a-file-in-powershell
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+function Unzip {
+    param([string]$zipfile, [string]$outpath)
+
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+
+Unzip $psToolsZip $psToolsPath
+
+# Move to sys32 so it can be used in cmd prompt
+Move-Item $psExecPath "C:\Windows\System32\psexec.exe"
+
+# Run psexec and pipe file to it
+& psexec -c KcsSetup.exe -n $timeout -s -v @file $listPath
+
+# Delete Temp folder 
+Move-Item "C:\Windows\System32\psexec.exe" $psExecPath
